45283e3db6c976f03f02a5f425cc5def173a2f37
deborah brouwer <3563deb6753@gmail.com>
2020-10-18 14:10:09 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="myproject",
+    version="0.0.1",
+    description="a test",
+    author="",
+    author_email="",
+    url="",
+    packages=find_packages(),
+)
